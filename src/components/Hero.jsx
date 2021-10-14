727af35b76c0aaf52670602ff3444aa0e5814a49
maliukalina <l.malyuk@gmail.com>
2021-10-14 16:46:41 -0400

    updated project

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./hero.css"
+
+function Hero() {
+  return (
+    <section className = "hero">
+      <h1>A Fresh Start To Your Day</h1>
+    </section>
+  )
+}
+
+
+export default Hero
