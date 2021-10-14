e326afc192601cdef94df1f6ddbb8d035b82996f
Dariel Mera <daridariel@yahoo.com>
2021-10-14 16:58:13 -0400

    the about section for jar coffee project

---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './about.css'
+
+function About() {
+    return (
+      <section>
+      <div className = "aboutWrapper">
+        <h2>About Us</h2>
+        <p>Stablish in 1972. JAR COFFEE offers the best coffee beans in the world. We export to more than 90 countries.</p>
+      </div>
+      </section>
+      
+    )
+  }
+  
+  export default About
